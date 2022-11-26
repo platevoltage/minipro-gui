e2c6aca21a3a7a606b18572e1ec3033b1a29f1ed
antieatingactivist <antieatingactivist@gmail.com>
2022-11-26 00:49:01 -0800

    added electron

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
+import { app, BrowserWindow, ipcMain, Menu, safeStorage } from 'electron';
 import * as util from 'util';
 import * as child_process from 'child_process';
 
 const exec = util.promisify(child_process.exec);
+
+app.whenReady().then(() => {
+
+  
+    const win = createMainWindow();
+    
+    
+});
+
+const createMainWindow = () => {
+    const win = new BrowserWindow({
+      width: 1280,
+      height: 720,
+      minHeight: 200,
+      minWidth: 600,
+    //   title: "Transmission",
+    //   frame: !isMac,
+    //   titleBarStyle: isMac ? "hidden" : "default",
+      webPreferences: {
+        enableBlinkFeatures: "CSSColorSchemeUARendering",
+        devTools: false,
+        // preload: path.join(__dirname, 'mainWindowPreload.js')
+      }
+    });
+
+    return win;
+}
 
 async function listDevices() {
     const { stdout } = await exec(`minipro -l -q TL866A`);
