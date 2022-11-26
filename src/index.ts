35461453b7401980ce66af65fb4f541ca76156cd
antieatingactivist <antieatingactivist@gmail.com>
2022-11-26 00:37:35 -0800

    added get device info

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,14 @@ import * as child_process from 'child_process';
 const exec = util.promisify(child_process.exec);
 
 async function listDevices() {
-    const { stdout } = await exec(`minipro -l`);
+    const { stdout } = await exec(`minipro -l -q TL866A`);
     console.log(stdout);
 }
 
-listDevices();
+async function getDeviceInfo(device: string) {
+    const { stderr } = await exec(`minipro -d ${device} -q TL866A`);
+    console.log(stderr);
+}
+
+getDeviceInfo("SST29SF512");
+// listDevices();
